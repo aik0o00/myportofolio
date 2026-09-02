9c640206fe4ffcbc3cbb43b74f1bfb596b50eeee
Aiko <aikozahwa0907@gmail.com>
2026-09-02 17:41:30 +0700

    tutorial 1 pbp

--- portofolio/settings.py
+++ portofolio/settings.py
@@ -48,8 +48,8 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
